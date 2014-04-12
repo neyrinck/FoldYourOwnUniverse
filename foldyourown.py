1194d85ef13f18be45dc48dfb0a934cc7fac71a9
Jonathan Merkle <jonbmerkle@gmail.com>
2014-04-12 17:10:07 -0400

    Updated foldyourown.py with latest version

--- foldyourown.py
+++ foldyourown.py
@@ -1,59 +1,63 @@
 import numpy as N, pylab as M
 
 def init():
-    d=N.loadtxt('/Users/neyrinck/plog/densmesh.txt')
-    dk = N.fft.rfftn(d*1e3)
-    
-    return dk
+    """ Set up the initial conditions """
+    density=N.loadtxt('/Users/neyrinck/plog/densmesh.txt')
+    # Real Fourier transform of "density" 
+    density_k = N.fft.rfftn(density*1e3) # 1e3 to enhance contrast
 
-def zeldovich(dk,boxsize=None):
-    sk = dk.shape
-    ng = sk[0]
+    psi = zeldovich(density_k)
+    # Zel'dovich displacement field
+
+    return density_k, psi
+
+def getkgrid(ng=64):
+    """ ng = number of particles in each dim """
+
+    thirdim=ng/2+1
+    sk = (ng,thirdim)
+
     kx = N.fromfunction(lambda x,y:x, sk).astype(N.float32)
     kx[N.where(kx > ng/2)] -= ng
     ky = N.fromfunction(lambda x,y:y, sk).astype(N.float32)
     ky[N.where(ky > ng/2)] -= ng
 
-    k2 = kx*kx + ky*ky
+    k2 = kx**2+ky**2
     k2[0,0] = 1.
-    kx /= k2
-    ky /= k2
-    pos = N.empty((ng,ng,2),dtype=N.float32)
+    return kx/k2, ky/k2
 
-    x=boxsize/ng*N.fromfunction(lambda x,y:x, (ng,ng))
-    #Inverse divergence; then displacement -> position
-    pos[:,:,0] = N.fft.irfftn(-1j*dk*kx) + x
-    pos[:,:,1] = N.fft.irfftn(-1j*dk*ky) + x.swapaxes(0,1)
-    return pos
+def zeldovich(dk,boxsize=None):
+    """ implements the Zel'dovich approximation to move particles """
+
+    sk = dk.shape #shape of the Fourier-space density field
+    ng = sk[0] #number of particles in each dimension
+
+    kx, ky = getkgrid(ng)
+
+    # psi = displacement of each particle from initial conditions.
+    psi = N.empty((ng,ng,2),dtype=N.float32)
+    psi[:,:,0] = N.fft.irfftn(-1j*dk*kx)
+    psi[:,:,1] = N.fft.irfftn(-1j*dk*ky)
+    return psi
 
 def psi2pos(psi,boxsize=500.):
-    x=boxsize/ng*N.fromfunction(lambda x,y:x, (ng,ng))
+    """ 
+    displacement field to positions 
+    (adding displacement to a regular lattice)
+    """
+    ng = psi.shape[0] # number of particles in each dimension
+
+    x=boxsize/ng*N.fromfunction(lambda x,y:x, (ng,ng)) 
+    # x positions on a regular lattice
+
     pos = 1.*psi
     pos[:,:,0] += x
     pos[:,:,1] += N.transpose(x)
-
-def invdiv(dk,boxsize=None):
-    sk = dk.shape
-    ng = sk[0]
-    kx = N.fromfunction(lambda x,y:x, sk).astype(N.float32)
-    kx[N.where(kx > ng/2)] -= ng
-    ky = N.fromfunction(lambda x,y:y, sk).astype(N.float32)
-    ky[N.where(ky > ng/2)] -= ng
-
-    k2 = kx*kx + ky*ky
-    k2[0,0] = 1.
-    kx /= k2
-    ky /= k2
-    pos = N.empty((ng,ng,2),dtype=N.float32)
-
-    x=boxsize/ng*N.fromfunction(lambda x,y:x, (ng,ng))
-    #Inverse divergence; then displacement -> position
-    pos[:,:,0] = N.fft.irfftn(-1j*dk*kx) + x
-    pos[:,:,1] = N.fft.irfftn(-1j*dk*ky) + x.swapaxes(0,1)
     return pos
 
-def plotvertices(dk,scale=1.):
-    pos = invdiv(dk*scale,boxsize=500.)
+def plotvertices(psi,scale=1.):
+    pos = psi2pos(psi*scale,boxsize=500.)
     
+    #plot the vertices
     M.scatter(pos[:,:,0].flat,pos[:,:,1].flat,s=1,lw=0)
 
