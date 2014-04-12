870f5bb11bc95b012f45d72277d886ab5ca5099f
Armen Kherlopian <ark2003@columbia.edu>
2014-04-12 18:27:36 -0400

    add user warning for argument count

--- run.py
+++ run.py
@@ -3,5 +3,11 @@ import sys
 import pylab as M
 
 dk, psi = foldyourown.init()
-foldyourown.plotvertices(psi,scale=float(sys.argv[1]))
-M.show()
+
+if len(sys.argv) == 2:
+    foldyourown.plotvertices(psi,scale=float(sys.argv[1]))
+    M.show()
+else:
+    print 'Usage: python run.py AgeOfUniverse'
+    print 'Valid Range for AgeOfUniverse: 0 - 100'
+    print 'AgeOfUniverse 1 is ~ 14B years'
